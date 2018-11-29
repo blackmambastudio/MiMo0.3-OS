a3d6a6c12e21a658337134a609f86decd6e70033
Andres Root <info@andresroot.co>
2018-11-28 23:39:12 -0500

    WIP mimo server

--- server/mimo.py
+++ server/mimo.py
@@ -10,3 +10,23 @@ socketio = SocketIO(app)
 @app.route('/')
 def index():
     render_template('index.html')
+
+
+def handle_serial(methods=['GET', 'POST']):
+    print('Received from serial IO')
+
+
+@socketio.on('connect2pi')
+def serial_read(json_data, methods=['GET', 'POST']):
+    print('Connected from {0}'.format(json_data))
+    response = {'message': 'Connected to serial'}
+
+
+
+
+
+
+
+
+
+
