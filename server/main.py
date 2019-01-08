71d148be12c852051ede695e321acf71e05dc66a
Andres Root <info@andresroot.co>
2019-01-08 17:04:32 -0500

    Add correct LCD screens code

--- server/main.py
+++ server/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import time
 from mimo.core import mimo_button
+from mimo.utils import I2C_LCD_driver
+
 # from mimo.mimoSerial import *
 from mimo.mimoPrinter import mimo_print, mimo_setup, mimo_init
 import RPi.GPIO as GPIO
@@ -15,6 +17,28 @@ socketio = SocketIO(app)
 colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF, 0xFFFFFF, 0x9400D3]
 pins = {'pin_R': 11, 'pin_G': 12, 'pin_B': 13}  # pins is a dict
 
+# Setup Pi IO for buttons
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Setup LCD screens
+lcd21 = I2C_LCD_driver.lcd(0x21)
+lcd22 = I2C_LCD_driver.lcd(0x22)
+lcd23 = I2C_LCD_driver.lcd(0x23)
+lcd25 = I2C_LCD_driver.lcd(0x25)
+lcd26 = I2C_LCD_driver.lcd(0x26)
+lcd27 = I2C_LCD_driver.lcd(0x27)
+
+SCREENS = {
+    21: lcd21,
+    22: lcd22,
+    23: lcd23,
+    25: lcd25,
+    26: lcd26,
+    27: lcd27
+}
+
 
 @app.route('/')
 def index():
@@ -25,8 +49,18 @@ def index():
 def test():
     return render_template('test.html')
 
+
 def handle_serial(methods=['GET', 'POST']):
     print('Received from serial IO')
+
+
+def handle_button(channel):
+    data = {}
+    socketio.emit('gpio', data)
+
+
+def display_lcd(i2c_address, message):
+    SCREENS[i2c_address].lcd_display_string(message, 1)
 
 
 def setup_pi():
@@ -109,4 +143,7 @@ def serial_read(json_data, methods=['GET', 'POST']):
 
 
 if __name__ == '__main__':
+    # Add button events
+    # GPIO.add_event_detect(12, GPIO.RISING, callback=handle_button, bouncetime=1000)
+    # GPIO.add_event_detect(16, GPIO.RISING, callback=handle_button, bouncetime=1000)
     socketio.run(app, host='0.0.0.0', debug=True)
