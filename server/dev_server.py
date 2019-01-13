7e84a654841a98dc06393b9c3f20f76888766327
Andres Root <info@andresroot.co>
2019-01-13 12:48:02 -0500

    add dev server for simple testing

---
+++ server/dev_server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1Secret!'
+socketio = SocketIO(app)
+
+
+def emit(data):
+    socketio.emit('gpio', data, callback=serial_read)
+
+
+@app.route('/test')
+def index():
+    return render_template('test.html')
+
+
+@app.route('/gpio/')
+def web_gpio():
+    button = request.args.get('button')
+    data = {
+        'button': button
+    }
+    emit(data)
+    return render_template('gpio.html', button=button)
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print('Message received')
+
+
+def serial_read(methods=['GET', 'POST']):
+    print('Message received serial')
+
+
+@socketio.on('connect2pi')
+def handle_serial(json_data, methods=['GET', 'POST']):
+    print('Connected from {0}'.format(json_data))
+    data = {'message': 'Connected to serial'}
+    socketio.emit('serial', data, callback=serial_read)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port='8000', debug=True)
