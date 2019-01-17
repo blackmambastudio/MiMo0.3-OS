7194135ddb79d9a99f2c5e8c9edc6693fbf2c434
Andres Root <info@andresroot.co>
2019-01-17 18:57:59 -0500

    WIP on IO

--- server/mimo/hardware/io.py
+++ server/mimo/hardware/io.py
@@ -8,8 +8,8 @@ buttons = OrderedDict()
 # buttons[11] = 'BTN_RED'
 
 # Material buttons
-buttons[12] = 'BTN_A'
-buttons[16] = 'BTN_B'
+buttons[12] = 'BTN-A'
+buttons[16] = 'BTN-B'
 # buttons[13] = 'BTN_C'
 # buttons[15] = 'BTN_D'
 # buttons[19] = 'BTN_E'
