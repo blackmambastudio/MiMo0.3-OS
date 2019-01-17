98fd0b11cd91352df4216ded8f0870999398c413
Andres Root <info@andresroot.co>
2019-01-17 16:33:31 -0500

    WIP on IO

--- server/rgb.py
+++ server/rgb.py
@@ -8,8 +8,10 @@ BLUE = 22
 
 GPIO.setup(RED, GPIO.OUT)
 GPIO.output(RED, 0)
+
 GPIO.setup(GREEN, GPIO.OUT)
 GPIO.output(GREEN, 0)
+
 GPIO.setup(BLUE, GPIO.OUT)
 GPIO.output(BLUE, 0)
 
@@ -18,5 +20,6 @@ try:
         GPIO.output(RED, int(1001))
         GPIO.output(GREEN, int(010))
         GPIO.output(BLUE, int(001))
+
 except KeyboardInterrupt:
     GPIO.cleanup()
