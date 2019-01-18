b0424f63dc4b796271c2de2af59d901cfee3d3ab
Andres Root <info@andresroot.co>
2019-01-17 23:08:53 -0500

    WIP on IO

--- server/buttons.py
+++ server/buttons.py
@@ -27,54 +27,57 @@ buttons[24] = {'msg': 'BTN-E', 'state': True}
 buttons[26] = {'msg': 'BTN-F', 'state': True}
 
 
-while True:
-    time.sleep(0.5)
+try:
+    while True:
+        time.sleep(0.5)
 
-    if GPIO.input(16) == GPIO.HIGH and buttons[16]['state']:
-        print('16')
-        GPIO.output(29, GPIO.HIGH)
-        buttons[16]['state'] = False
-    elif not(GPIO.input(16) == GPIO.HIGH and buttons[16]['state']):
-        GPIO.output(29, GPIO.LOW)
-        buttons[16]['state'] = True
+        if GPIO.input(16) == GPIO.HIGH and buttons[16]['state']:
+            print('16')
+            GPIO.output(29, GPIO.HIGH)
+            buttons[16]['state'] = False
+        elif not(GPIO.input(16) == GPIO.HIGH and buttons[16]['state']):
+            GPIO.output(29, GPIO.LOW)
+            buttons[16]['state'] = True
 
-    if GPIO.input(24) == GPIO.HIGH and buttons[24]['state']:
-        print('24')
-        GPIO.output(35, GPIO.HIGH)
-        buttons[24]['state'] = False
-    elif not(GPIO.input(24) == GPIO.HIGH and buttons[24]['state']):
-        GPIO.output(35, GPIO.LOW)
-        buttons[24]['state'] = True
-
-    # if GPIO.input(18) == GPIO.HIGH:
-    #     print(18)
-    #     if buttons[18]['state']:
-    #         GPIO.output(31, GPIO.HIGH)
-    #         buttons[18]['state'] = False
-    #     else:
-    #         GPIO.output(31, GPIO.LOW)
-    #         buttons[18]['state'] = True
-    # if GPIO.input(22) == GPIO.HIGH:
-    #     print(22)
-    #     if buttons[22]['state']:
-    #         GPIO.output(33, GPIO.HIGH)
-    #         buttons[22]['state'] = False
-    #     else:
-    #         GPIO.output(33, GPIO.LOW)
-    #         buttons[22]['state'] = True
-    # if GPIO.input(24) == GPIO.HIGH:
-    #     print(24)
-    #     if buttons[24]['state']:
-    #         GPIO.output(35, GPIO.HIGH)
-    #         buttons[24]['state'] = False
-    #     else:
-    #         GPIO.output(35, GPIO.LOW)
-    #         buttons[24]['state'] = True
-    # if GPIO.input(26) == GPIO.HIGH:
-    #     print(26)
-    #     if buttons[26]['state']:
-    #         GPIO.output(37, GPIO.HIGH)
-    #         buttons[26]['state'] = False
-    #     else:
-    #         GPIO.output(37, GPIO.LOW)
-    #         buttons[26]['state'] = True
+        if GPIO.input(24) == GPIO.HIGH and buttons[24]['state']:
+            print('24')
+            GPIO.output(35, GPIO.HIGH)
+            buttons[24]['state'] = False
+        elif not(GPIO.input(24) == GPIO.HIGH and buttons[24]['state']):
+            GPIO.output(35, GPIO.LOW)
+            buttons[24]['state'] = True
+       
+        # if GPIO.input(18) == GPIO.HIGH:
+        #     print(18)
+        #     if buttons[18]['state']:
+        #         GPIO.output(31, GPIO.HIGH)
+        #         buttons[18]['state'] = False
+        #     else:
+        #         GPIO.output(31, GPIO.LOW)
+        #         buttons[18]['state'] = True
+        # if GPIO.input(22) == GPIO.HIGH:
+        #     print(22)
+        #     if buttons[22]['state']:
+        #         GPIO.output(33, GPIO.HIGH)
+        #         buttons[22]['state'] = False
+        #     else:
+        #         GPIO.output(33, GPIO.LOW)
+        #         buttons[22]['state'] = True
+        # if GPIO.input(24) == GPIO.HIGH:
+        #     print(24)
+        #     if buttons[24]['state']:
+        #         GPIO.output(35, GPIO.HIGH)
+        #         buttons[24]['state'] = False
+        #     else:
+        #         GPIO.output(35, GPIO.LOW)
+        #         buttons[24]['state'] = True
+        # if GPIO.input(26) == GPIO.HIGH:
+        #     print(26)
+        #     if buttons[26]['state']:
+        #         GPIO.output(37, GPIO.HIGH)
+        #         buttons[26]['state'] = False
+        #     else:
+        #         GPIO.output(37, GPIO.LOW)
+        #         buttons[26]['state'] = True
+except KeyboardInterrupt:
+    GPIO.cleanup()
